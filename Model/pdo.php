46b9a19f89e1512f981164bdbc724e0622b008f4
Phạm Chình <chinhpph35141@fpt.edu.vn>
2023-11-24 15:00:22 +0700

    Xây dưng  mô hình mvc

---
+++ Model/pdo.php
@@ -0,0 +1,68 @@
+<?php
+function pdo_get_connection(){
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname="du_an_1";
+    try {
+        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $conn;
+    } catch(PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
+}
+//CUD
+function pdo_execute($sql){
+    $sql_args=array_slice(func_get_args(),1);
+    try{
+        $conn=pdo_get_connection();
+        $stmt=$conn->prepare($sql);
+        $stmt->execute($sql_args);
+
+    }
+    catch(PDOException $e){
+        throw $e;
+    }
+    finally{
+        unset($conn);
+    }
+}
+// READS
+function pdo_query($sql){
+    $sql_args=array_slice(func_get_args(),1);
+
+    try{
+        $conn=pdo_get_connection();
+        $stmt=$conn->prepare($sql);
+        $stmt->execute($sql_args);
+        $rows=$stmt->fetchAll();
+        return $rows;
+    }
+    catch(PDOException $e){
+        throw $e;
+    }
+    finally{
+        unset($conn);
+    }
+}
+// READ
+function pdo_query_one($sql){
+    $sql_args=array_slice(func_get_args(),1);
+    try{
+        $conn=pdo_get_connection();
+        $stmt=$conn->prepare($sql);
+        $stmt->execute($sql_args);
+        $row=$stmt->fetch(PDO::FETCH_ASSOC);
+        // Đọc và hiển thị giá trị trong danh sách trả về
+        return $row;
+    }
+    catch(PDOException $e){
+        throw $e;
+    }
+    finally{
+        unset($conn);
+    }
+}
+pdo_get_connection();
+?>
