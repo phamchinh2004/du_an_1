b143998349d280711b9c730fc41b6938bdab1da1
dson273 <dacson273@gmail.com>
2023-11-27 20:55:18 +0700

    update

--- View/admin/sanpham/list.php
+++ View/admin/sanpham/list.php
@@ -1,8 +1,77 @@
-<main class="content">
+<div class="content">
     <h1>Danh sách sản phẩm</h1>
-</main>
-<script src="https://kit.fontawesome.com/4392bd821c.js" crossorigin="anonymous"></script>
-<!--Crossorigin: Nguồn gốc chéo, Anonymous: Vô danh-->
+    <form action="" method="POST">
+        <div class="listok">
+            <input type="text" placeholder="Tìm kiếm sản phẩm" name="kyw">
+            <select name="iddm" id="">
+                <option value="0">Danh mục</option>
+            </select>
+            <input type="submit" value="Lọc" name="listok">
+        </div>
+    </form>
+    <a href="index.php?act=addsp"> <input class="spadm" type="button" value="Thêm sản phẩm"></a>
+    <div class="form_content ">
+        <form action="" method="POST">
+            <div class="form_loai ">
+                <table>
+                    <tr>
+                        <th>ID</th>
+                        <th>Tên sản phẩm</th>
+                        <th>Hình ảnh</th>
+                        <th>Giá</th>
+                        <th>Số lượng</th>
+                        <th>Lượt xem</th>
+                        <th>Lượt mua</th>
+                        <th>Lượt đánh giá</th>
+                        <th></th>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Iphone</td>
+                        <td>Ảnh</td>
+                        <td>17.000.000VNĐ</td>
+                        <td>100</td>
+                        <td>100</td>
+                        <td>10</td>
+                        <td>10</td>
+                        <td>
+                            <a href="suasp.php"> <input type="button" value="Sửa"> </a> 
+                            <a href="xoasp.php"> <input type="button" value="Xóa"> </a> 
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>2</td>
+                        <td>Iphone</td>
+                        <td>Ảnh</td>
+                        <td>17.000.000VNĐ</td>
+                        <td>100</td>
+                        <td>100</td>
+                        <td>10</td>
+                        <td>10</td>
+                        <td>
+                            <a href="suasp.php"> <input type="button" value="Sửa"> </a> 
+                            <a href="xoasp.php"> <input type="button" value="Xóa"> </a> 
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>3</td>
+                        <td>Iphone</td>
+                        <td>Ảnh</td>
+                        <td>17.000.000VNĐ</td>
+                        <td>100</td>
+                        <td>100</td>
+                        <td>10</td>
+                        <td>10</td>
+                        <td>
+                            <a href="suasp.php"> <input type="button" value="Sửa"> </a> 
+                            <a href="xoasp.php"> <input type="button" value="Xóa"> </a> 
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </form>
+    </div>
+</div>
 </body>
 
 </html>
