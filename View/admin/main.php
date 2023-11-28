d85f12467682dd99b79f71753e861bc032c0ea2d
Phạm Chình <chinhpph35141@fpt.edu.vn>
2023-11-28 16:51:46 +0700

    update

--- View/admin/main.php
+++ View/admin/main.php
@@ -1,48 +1,5 @@
 <main class="content">
-    <h1>Trang chủ Lorem ipsum dolor sit amet consectetur adipisici
-        ng elit. Animi doloremque recusandae nesciunt consequatur, e
-        os, numquam, cum distinctio ipsum natus dignissimos eius mo
-        llitia non? Ab, esse aperiam porro sint maiores reprehenderit.</h1>
-    <h1>Trang chủ Lorem ipsum dolor sit amet consectetur adipisici
-        ng elit. Animi doloremque recusandae nesciunt consequatur, e
-        os, numquam, cum distinctio ipsum natus dignissimos eius mo
-        llitia non? Ab, esse aperiam porro sint maiores reprehenderit.</h1>
-    <h1>Trang chủ Lorem ipsum dolor sit amet consectetur adipisici
-        ng elit. Animi doloremque recusandae nesciunt consequatur, e
-        os, numquam, cum distinctio ipsum natus dignissimos eius mo
-        llitia non? Ab, esse aperiam porro sint maiores reprehenderit.</h1>
-    <h1>Trang chủ Lorem ipsum dolor sit amet consectetur adipisici
-        ng elit. Animi doloremque recusandae nesciunt consequatur, e
-        os, numquam, cum distinctio ipsum natus dignissimos eius mo
-        llitia non? Ab, esse aperiam porro sint maiores reprehenderit.</h1>
-    <h1>Trang chủ Lorem ipsum dolor sit amet consectetur adipisici
-        ng elit. Animi doloremque recusandae nesciunt consequatur, e
-        os, numquam, cum distinctio ipsum natus dignissimos eius mo
-        llitia non? Ab, esse aperiam porro sint maiores reprehenderit.</h1>
-    <h1>Trang chủ Lorem ipsum dolor sit amet consectetur adipisici
-        ng elit. Animi doloremque recusandae nesciunt consequatur, e
-        os, numquam, cum distinctio ipsum natus dignissimos eius mo
-        llitia non? Ab, esse aperiam porro sint maiores reprehenderit.</h1>
-    <h1>Trang chủ Lorem ipsum dolor sit amet consectetur adipisici
-        ng elit. Animi doloremque recusandae nesciunt consequatur, e
-        os, numquam, cum distinctio ipsum natus dignissimos eius mo
-        llitia non? Ab, esse aperiam porro sint maiores reprehenderit.</h1>
-    <h1>Trang chủ Lorem ipsum dolor sit amet consectetur adipisici
-        ng elit. Animi doloremque recusandae nesciunt consequatur, e
-        os, numquam, cum distinctio ipsum natus dignissimos eius mo
-        llitia non? Ab, esse aperiam porro sint maiores reprehenderit.</h1>
-    <h1>Trang chủ Lorem ipsum dolor sit amet consectetur adipisici
-        ng elit. Animi doloremque recusandae nesciunt consequatur, e
-        os, numquam, cum distinctio ipsum natus dignissimos eius mo
-        llitia non? Ab, esse aperiam porro sint maiores reprehenderit.</h1>
-    <h1>Trang chủ Lorem ipsum dolor sit amet consectetur adipisici
-        ng elit. Animi doloremque recusandae nesciunt consequatur, e
-        os, numquam, cum distinctio ipsum natus dignissimos eius mo
-        llitia non? Ab, esse aperiam porro sint maiores reprehenderit.</h1>
-    <h1>Trang chủ Lorem ipsum dolor sit amet consectetur adipisici
-        ng elit. Animi doloremque recusandae nesciunt consequatur, e
-        os, numquam, cum distinctio ipsum natus dignissimos eius mo
-        llitia non? Ab, esse aperiam porro sint maiores reprehenderit.</h1>
+    <h1>Trang chủ </h1>
 
 </main>
 <script src="https://kit.fontawesome.com/4392bd821c.js" crossorigin="anonymous"></script>
