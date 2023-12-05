1ae263d27629915b6f18789295371d27ee292c92
dson273 <dacson273@gmail.com>
2023-12-05 22:10:05 +0700

    update trang buysuccess

---
+++ View/buysuccess.php
@@ -0,0 +1,31 @@
+<div class="page-buysuccess">
+    <div class="buysuccess-title mb">
+        <h2>ĐẶT HÀNG THÀNH CÔNG</h2>
+        <i class="fa-solid fa-circle-check"></i>
+        <p>Cảm ơn quý khách đã tin tưởng mua hàng tại SAN'S MOBILE</p>
+    </div>
+    <div class="box-thongtinkh">
+        <div class="content-thongtinkh mb">
+            <h3>Thông tin đặt hàng</h3>
+            <table>
+                <tr>
+                    <th>Mã đơn hàng:</th>
+                    <td>123432</td>
+                </tr>
+                <tr>
+                    <th>Hình thức thanh toán:</th>
+                    <td>Thanh toán sau khi nhận hàng</td>
+                </tr>
+                <tr>
+                    <th>Họ tên khách hàng:</th>
+                    <td>Nguyen Van A</td>
+                </tr>
+                <tr>
+                    <th>Số điện thoại:</th>
+                    <td>0987654321</td>
+                </tr>
+            </table>
+        </div>
+        <a href="index.php?act=sptrangchu"><input class="" type="button" value="MUA THÊM SẢN PHẨM KHÁC"></a>
+    </div>
+</div>
